7e8ee9785a36f84e3979c4625aa16fb93a65a99e
Justin Poliey <justin@getglue.com>
2012-03-19 19:25:01 -0400

    deleted the tests directory

---
+++ tests/basic_test.php
@@ -0,0 +1,12 @@
+<?php
+namespace redisent\tests\units;
+
+require_once '../mageekguy.atoum.phar';
+require_once '../redisent.php';
+
+use mageekguy\atoum;
+
+class BasicTest extends atoum\test {
+  
+
+}
