f474d1e5a6cb55a0953f76aaff939f30913c5cea
Mardix <mcx2082@gmail.com>
2013-01-03 23:46:33 -0500

    Added the Exception class

---
+++ src/Redisent/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @name Exception
+ * @author Mardix - https://github.com/mardix
+ * @since Jan 3 2013
+ */
+
+namespace Redisent;
+
+use Exception;
+
+class Exception extends Exception
+{
+}
