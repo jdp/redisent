19764aee5c2f5dbc48157eb71f69e7899eab52c2
Thijs Feryn <thijs@combellgroup.com>
2014-04-03 14:39:25 +0200

    Added docs

--- Rwsplit.php
+++ Rwsplit.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Credis_Rwsplit
+ *
+ * Handles read/write splitting of Redis commands. Is used in Credis_Cluster to send write requests to the master server
+ * and read requests to one of the slave servers
+ *
+ * @author Thijs Feryn <thijs@feryn.eu>
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @package Credis_Sentinel
+ */
 class Credis_Rwsplit
 {
     protected static $_commands = array(
