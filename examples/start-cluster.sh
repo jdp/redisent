c221c2c2ce049cc9422ef0f6388bfad9c503b8b8
Thijs Feryn <thijs@combellgroup.com>
2014-05-06 12:47:34 +0200

    Remove examples in favor of good documentation

--- examples/start-cluster.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-redis-server redis-master.conf
-redis-server redis-master2.conf
--- examples/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for file in `find ./ -name "redis-*.pid"`
-do
-    kill `cat "$file"`
-done
-rm -f dump.rdb
