692419340f5dbd2481feacd7e40f868c4247c54a
Colin Mollenhour <colin@mollenhour.com>
2015-12-17 12:15:30 -0500

    Fix cluster calling non-magic methods on clients. Fixes #40

--- Cluster.php
+++ Cluster.php
@@ -191,7 +191,7 @@ class Credis_Cluster
     $name = array_shift($args);
     $results = array();
     foreach($this->clients as $client) {
-      $results[] = $client->__call($name, $args);
+      $results[] = call_user_func_array([$client, $name], $args);
     }
     return $results;
   }
@@ -224,7 +224,7 @@ class Credis_Cluster
     else {
       $client = $this->byHash($args[0]);
     }
-    return $client->__call($name, $args);
+    return call_user_func_array([$client, $name], $args);
   }
 
   /**
