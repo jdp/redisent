9ba93c1ce34ded47bcedab16b5d49a3353f52aa1
Xon <tazipper1@hotmail.com>
2017-09-02 08:33:54 +0800

    Add some Sorted Sets phpdocs

--- Client.php
+++ Client.php
@@ -138,12 +138,20 @@ class CredisException extends Exception
  * @method int           rPushX(string $key, mixed $value)
  *
  * Sorted Sets:
+ * @method int           zAdd(string $key, double $score, string $value)
  * @method int           zCard(string $key)
+ * @method int           zSize(string $key)
+ * @method int           zCount(string $key, mixed $start, mixed $stop)
+ * @method int           zIncrBy(string $key, double $value, string member)
  * @method array         zRangeByScore(string $key, mixed $start, mixed $stop, array $args = null)
  * @method array         zRevRangeByScore(string $key, mixed $start, mixed $stop, array $args = null)
  * @method int           zRemRangeByScore(string $key, mixed $start, mixed $stop)
  * @method array         zRange(string $key, mixed $start, mixed $stop, array $args = null)
  * @method array         zRevRange(string $key, mixed $start, mixed $stop, array $args = null)
+ * @method int           zRank(string $key, string member)
+ * @method int           zRevRank(string $key, string member)
+ * @method int           zRem(string $key, string member)
+ * @method int           zDelete(string $key, string member)
  * TODO
  *
  * Pub/Sub
@@ -909,6 +917,12 @@ class Credis_Client {
                         }
                         $args[1] = $cArgs;
                     }
+                case 'zsize':
+                    $name = 'zcard';
+                    break;
+                case 'zdelete':
+                    $name = 'zrem';
+                    break;
             }
             // Flatten arguments
             $args = self::_flattenArguments($args);
